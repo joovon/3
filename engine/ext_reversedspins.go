b4569c4a71d508fe88d64cff4273265f19240c54
joovon <29384177+joovon@users.noreply.github.com>
2022-04-21 19:40:07 +0200

    Added reversedspins extension

---
+++ engine/ext_reversedspins.go
@@ -0,0 +1,24 @@
+package engine
+
+import (
+	"math"
+
+	"github.com/mumax/3/cuda"
+	"github.com/mumax/3/data"
+)
+
+var (
+	Ext_ReversedSpins        = NewScalarValue("ext_reversedspins", "", "Reversed spins", GetReversedSpins)
+	Ext_ReversedSpinsDensity = NewScalarField("ext_reversedspins", "1", "Reversed spins density", SetReversedSpinsDensity)
+)
+
+func SetReversedSpinsDensity(dst *data.Slice) {
+	cuda.SetReversedSpins(dst, M.Buffer(), M.Mesh())
+}
+
+func GetReversedSpins() float64 {
+	s := ValueOf(Ext_ReversedSpinsDensity)
+	defer cuda.Recycle(s)
+	N := Mesh().Size()
+	return (1.0 / float64(N[Z])) * float64(cuda.Sum(s))
+}
